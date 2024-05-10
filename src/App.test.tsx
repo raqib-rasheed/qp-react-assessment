202673262082e3e2f37383108bb0e6b8a41f6d91
raqib <raqib.rasheed@techverito.com>
2024-05-11 00:23:08 +0530

    project setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const helloText = screen.getByText(/Hello World!/i);
+
+  expect(helloText).toBeInTheDocument();
 });
