3ab7eb5e9c42467d14ef5279a2d96a52911d59bc
raqib <raqib.rasheed@techverito.com>
2024-05-11 03:42:40 +0530

    add: AddTodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { AddTodo } from "./components/add-todo";
 
 function App() {
   return (
     <div className="App">
       <h1>Todo App</h1>
+      <AddTodo />
     </div>
   );
 }
