4235a51eee43a020bb5191b37469e4d285686f26
raqib <raqib.rasheed@techverito.com>
2024-05-12 01:11:17 +0530

    update: README.md

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import { TodoList } from "./components/todo-list";
 import { AddTodo } from "./components/add-todo";
 import { RootState } from "./store/store";
 import { useSelector } from "react-redux";
+// import { mockTodo } from './mocks/todoList.mock';
 
 function App() {
+  // To test large todo set gets rendered, comment line number 13 &
+  // Uncomment line number 14.
+
   const todos = useSelector((state: RootState) => state.todos);
+  // const todos = new Array(1500).fill(mockTodo);
+
   return (
     <div className="App">
       <h1>Todo App</h1>
