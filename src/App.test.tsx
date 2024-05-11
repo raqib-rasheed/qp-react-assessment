c6cf826b86c4591cb30b5c2768e268d80bcbd3b8
raqib <raqib.rasheed@techverito.com>
2024-05-11 19:58:52 +0530

    configure: redux store to add new todo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 describe("App component", () => {
   test("Should show 'Todo App' heading", () => {
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
 
     const heading = screen.getByText(/Todo App/i);
 
@@ -11,7 +17,11 @@ describe("App component", () => {
   });
 
   test("Should show input to add new todo", () => {
-    render(<App />);
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
 
     const addNewInput = screen.getByPlaceholderText(/Add a new task/i);
 
---
+++ src/store/todoSlices.ts
@@ -0,0 +1,34 @@
+// features/todosSlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TodosState {
+  todos: Todo[];
+}
+
+const initialState: TodosState = {
+  todos: [],
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+      state.todos.push(newTodo);
+    },
+  },
+});
+
+export const { addTodo } = todosSlice.actions;
+export default todosSlice.reducer;
